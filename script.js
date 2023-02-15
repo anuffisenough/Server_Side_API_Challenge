ac23eba2a637145ad818317910243f1c326b1720
anuffisenough <uffmanch@gmail.com>
2023-02-15 15:23:27 -0700

    Adds functionality to pull more weather data, convert as needed

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
     
 var submitBtn = $("#submit");
+var searchListDiv = $("#search-div");
+var savedCityButton = $('<button>');
+var cityName = $("#city-search").val();
+var today = dayjs();
+var currentForecastDiv = $("#todays-forecast");
 
 //Function uses OpenWeatherAPI to get weather data
 
@@ -26,6 +31,16 @@ var submitBtn = $("#submit");
              })
                  .then(function (data) {
                     console.log(data);
+//Saves current weather details and icon
+             var currentHumidity = data.list[0].main.humidity;
+             var currentTempF = ((((data.list[0].main.temp)-273.15)*9)/5)+ 32;
+             var currentWind = data.list[0].wind.speed;
+             var currentWeatherIcon = data.list[0].weather[0].icon;
+
+            console.log(currentWeatherIcon);
+            console.log(currentTempF + " degrees F");
+            console.log(currentHumidity + "%");
+            console.log(currentWind + "MPH");
                  
              }) 
         });
@@ -33,7 +48,11 @@ var submitBtn = $("#submit");
 }
 
 
-//Event listener on city search submit button, calls getWeather function
+//Event listener on city search submit button, calls getWeather function, creates button for saved search future reference
  submitBtn.on("click", function() {
         getWeather();
+        savedCityButton.text($("#city-search").val());
+        searchListDiv.append(savedCityButton);
+       // var currentForecastHeading = $("<h3>").text($("#city-search" + today.format("dd MM YYYY")).val());
+       // currentForecastDiv.append(currentForecastHeading);
  });
